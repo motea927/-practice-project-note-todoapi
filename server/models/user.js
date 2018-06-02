a108a64e80a0473869ac68d49ba0705feade2059
motea927 <motea927@yahoo.com.tw>
2018-06-02 16:28:41 +0800

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function (){
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    })
+}
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
@@ -73,6 +83,8 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     })
 };
+
+
 
 UserSchema.statics.findByCredentials = function (email, password) {
 
